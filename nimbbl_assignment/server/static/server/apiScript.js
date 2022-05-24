da43ef126a743b5a70e4f63c597d2651b2d6c212
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-05-24 23:18:17 +0530

    added static site
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

---
+++ nimbbl_assignment/server/static/server/apiScript.js
@@ -0,0 +1,13 @@
+function buy(){
+    window.alert('TODO');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.addEventListener('click', event => {
+        
+        const element = event.target;
+
+        
+    })
+})
